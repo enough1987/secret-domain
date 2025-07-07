a4ceba17c63906e3b0ccde51cd0280ed17297e48
oleksandr.tilha <oleksandr.tilha@nordstrom.com>
2025-07-07 17:04:10 +0300

    feat: change deploy. server goes to EC2, frontend goes to S3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://todo-app2025-frontend.s3-website.eu-north-1.amazonaws.com', 
+      'http://localhost:3000'
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap().catch((err) => {
