86a59cf96da7d90697e7532716cba9d8de20e9ea
oleksandr.tilha <oleksandr.tilha@nordstrom.com>
2025-07-08 22:18:49 +0300

    feat: add verion

--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import packageJson from '../../../package.json'
 
 const Home: React.FC = () => {
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">Home</h1>
       <p className="text-gray-700">Welcome to the Home page!</p>
+      <p className="text-gray-700">Version {packageJson.version}</p>
     </div>
   )
 }
