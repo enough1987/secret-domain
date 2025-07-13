be8105b8cf20122e2fe06bf1816d537a9b6e26ae
oleksandr.tilha <tilgaaleksandr@gmail.com>
2025-07-13 11:16:25 +0300

    feat: add build vetion

--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import packageJson from '../../../package.json'
 
+const buildVersion = import.meta.env.VITE_BUILD_VERSION
+
 const Home: React.FC = () => {
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">Home</h1>
       <p className="text-gray-700">Welcome to the Home page!</p>
       <p className="text-gray-700">Version {packageJson.version}</p>
+      <p className="text-gray-700">Build Version {buildVersion}</p>
     </div>
   )
 }
