e263a7b29712c8f28d63ca0c91ee772708187240
oleksandr.tilha <tilgaaleksandr@gmail.com>
2025-07-23 20:07:39 +0300

    feat: update

--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -12,11 +12,7 @@ const Home: React.FC = () => {
       <h1 className="text-2xl font-bold mb-4">Home</h1>
       <p className="text-gray-700">Welcome to the Home page!</p>
       <p className="text-gray-700">Version {packageJson.version}</p>
-      {
-        healthData && (
-          <p className="text-gray-700">Health DB Check: {healthData?.db ? 'Healthy' : 'Unhealthy'}</p>
-        )
-      }
+      <p className="text-gray-700">Health DB Check: {healthData?.db ? 'Healthy' : 'Unhealthy'}</p>
       {
         buildVersion && (
           <p className="text-gray-700">Build Version: {buildVersion}</p>
